6d16011eb29cd110382c5cc76e6832e3ae48a487
www42 <thomas.jaekel@gfn.training>
2022-04-27 10:51:48 +0200

    Add uebung5

---
+++ Übung 5.ps1
@@ -0,0 +1,15 @@
+﻿# Welche Version des .Net Frameworks ist installiert?
+#
+# Tip: HKLM:\SOFTWARE\Microsoft\NET Framework Setup\NDP\v4\Full
+#      Property 'Release'
+#
+#      Tabelle https://msdn.microsoft.com/en-us/library/hh925568(v=vs.110)
+
+
+
+# Bei dem Benutzer den Registry Key\Software\Scripts anlegen 
+# Dort den Eintrag "PSDriveName" (String) anlegen mit dem Wert "ScriptShare"
+
+
+
+# Welche Root-Zertifikate (Benutzer) sind abgelaufen?
