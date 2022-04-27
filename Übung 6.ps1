c1467e5189469ea076c1f62e994e2272afdbd08e
www42 <thomas.jaekel@gfn.training>
2022-04-27 15:01:44 +0200

    Add uebung6

---
+++ Übung 6.ps1
@@ -0,0 +1,13 @@
+﻿# WMI Datenbank abfragen
+#-----------------------
+
+# Die Serien-Nummer vom BIOS ermitteln.
+
+
+# Alle Dienste ermitteln, die nicht laufen, die aber den Starttype  Automatisch haben. 
+
+
+
+# Zu jedem Dienst die zugehörigen Prozesse ermitteln.
+
+
