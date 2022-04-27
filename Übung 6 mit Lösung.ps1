fee02c98db2f3d7b5183f13a7d0e40af64fed9b2
www42 <thomas.jaekel@gfn.training>
2022-04-27 17:06:30 +0200

    Loesung 6

---
+++ Übung 6 mit Lösung.ps1
@@ -0,0 +1,23 @@
+﻿# WMI Datenbank abfragen
+#-----------------------
+
+# Die Serien-Nummer vom BIOS ermitteln.
+Get-CimInstance -ClassName Win32_BIOS | Select-Object -Property SerialNumber
+
+
+
+# Alle Dienste ermitteln, die nicht laufen, die aber den Starttype  Automatisch haben. 
+Get-CimInstance -ClassName Win32_Service | Where-Object {$_.State -ne "running" -and $_.StartMode -eq "auto"}
+
+
+
+# Zu jedem Dienst die zugehörigen Prozesse ermitteln.
+
+Get-CimInstance -ClassName Win32_Service | where State -EQ "running" |
+sort Name |
+Format-Table `
+    @{l="ServiceName";
+      e={$_.Name}}, `
+    @{l="ProcessName";
+      e={(Get-Process -Id $_.processId).Name}}, `
+    ProcessId
