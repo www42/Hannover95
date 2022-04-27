dca55115a2bb134a767d752f422ba11f86a0365b
www42 <thomas.jaekel@gfn.training>
2022-04-27 12:52:02 +0200

    Add uebung5

---
+++ Übung 5 mit Lösung.ps1
@@ -0,0 +1,19 @@
+﻿# Welche Version des .Net Frameworks ist installiert?
+# Tip: HKLM:\SOFTWARE\Microsoft\NET Framework Setup\NDP\v4\Full
+Get-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\NET Framework Setup\NDP\v4\Full"
+Get-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\NET Framework Setup\NDP\v4\Full" | Select-Object -Property Version
+
+
+
+# Bei dem Benutzer den Registry Key\Software\Scripts anlegen 
+# Dort den Eintrag "PSDriveName" (String) anlegen mit dem Wert "ScriptShare"
+
+New-Item –Path HKCU:\Software –Name "Scripts"
+New-ItemProperty -Path HKCU:\Software\Scripts -Name "PSDriveName" –Value "ScriptShare"
+
+
+# Welche Root-Zertifikate (Benutzer) sind abgelaufen?
+dir Cert:\CurrentUser\Root
+dir Cert:\CurrentUser\Root | Get-Member -Name NotAfter
+$Today = Get-Date
+dir Cert:\CurrentUser\Root | Where-Object NotAfter -LT $Today | Sort-Object -Property NotAfter | Format-List Subject,NotAfter
