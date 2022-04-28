8fad16fb9e3097621fc27c6bb6a827e7ba5b0348
www42 <thomas.jaekel@gfn.training>
2022-04-28 08:33:52 +0200

    Add uebung7

---
+++ Übung 7.ps1
@@ -0,0 +1,38 @@
+# Finde alle AD Benutzer der Marketing Abteilung. Nutze dabei den Befehl
+
+
+# Speichere alle Benutzer der Marketing Abteilung in eine Variable. Nutze die Variable $MarketingUsers
+
+
+# Welchen Typ hat $MarketingUsers?
+
+
+# Wieviele Benutzer der Marketing Abteilung gibt es?
+
+
+
+# Wie heißt der erste Benutzer der Marketing Abteilung?
+
+
+
+# Die Firma strukturiert sich neu! Die Marketing Abteilung gehört jetzt zum Business Development.
+# Ändere bei Benutzern die Abteilung von "Marketing" in "Business Development"!
+
+
+# Hat's geklappt?
+
+
+
+
+# Erzeuge ein Array $Computers und schreibe die Namen "LON-DC1" und "LON-DC2" hinein!
+
+
+# Füge den Name "LON-SVR1" zum Array hinzu!
+
+
+# Entferne "LON-DC2" vom Array!
+
+
+# Teste die Netzwerkverbindung zu den Computern im Array!
+
+
